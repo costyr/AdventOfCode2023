698e6634c05aa81cc96a87fe74cfc441d4689250
Costi Rada <costi.rada@caphyon.com>
2023-12-05 21:17:05 +0200

    Day 5 second problem solution

--- Day5.js
+++ Day5.js
@@ -1,29 +1,33 @@
 const util = require('./Util.js');
 
+function GenerateSeeds(aStart, aCount) {
+  let seeds = [];
+  for (let i = 0; i < aCount; i++)
+    seeds.push(aStart + i);
+  return seeds;
+}
+
 function TransformSeeds(aSeeds, aTransformMap, transformOrder) {
-  let locations = aSeeds.map((aSeed)=>{
+  let locations = aSeeds.map((aSeed) => {
 
     let nr = aSeed;
     console.log("-------------------------");
-    for (let i = 0; i < transformOrder.length; i++)
-    {
+    for (let i = 0; i < transformOrder.length; i++) {
       let tt = aTransformMap.get(transformOrder[i]);
 
       let found = false;
-      for (let j = 0; j < tt.length; j++)
-      {
+      for (let j = 0; j < tt.length; j++) {
         let oo = tt[j];
         let maxValue = (oo[1] + oo[2]);
-        if (nr >= oo[1] && nr < maxValue)
-        {
+        if (nr >= oo[1] && nr < maxValue) {
           let offset = (nr - oo[1]);
           let bb = oo[0] + offset;
 
           console.log(transformOrder[i] + ": " + nr + " => " + bb);
           nr = bb;
-          found = true;         
-          break; 
-        }  
+          found = true;
+          break;
+        }
       }
       if (!found)
         console.log(transformOrder[i] + ": " + nr + " => " + nr);
@@ -34,76 +38,106 @@ function TransformSeeds(aSeeds, aTransformMap, transformOrder) {
 
   console.log(locations);
 
-  return locations.reduce((aMin, aElem)=>{ 
+  return locations.reduce((aMin, aElem) => {
     if (aElem < aMin)
       return aElem;
     return aMin;
   }, Number.MAX_SAFE_INTEGER);
 }
 
-function TransformInterval(aSeedStart, aSeedEnd, aTransformMap, transformOrder) {
+function TransformInterval(aSeedStart, aSeedEnd, aTransformMap, aTransformOrder, aTransform, aMin) {
+
+  if (aTransform >= aTransformOrder.length) {
+    if (aSeedStart < aMin.min)
+      aMin.min = aSeedStart;
+    return;
+  }
 
   let start = aSeedStart;
   let end = aSeedEnd;
   console.log("-------------------------");
-  for (let i = 0; i < transformOrder.length; i++)
-  {
-    let tt = aTransformMap.get(transformOrder[i]);
+  let tt = aTransformMap.get(aTransformOrder[aTransform]);
 
-    let found = false;
-    for (let j = 0; j < tt.length; j++)
-    {
-      let oo = tt[j];
-      let maxValue = (oo[1] + oo[2]);
-      let maxEnd = start + end;
-      if (start >= oo[1] && maxEnd < maxValue)
-      {
-        let offset = (start - oo[1]);
-        let newStart = oo[0] + offset;
-          
-        console.log(transformOrder[i] + ": [" + start + "," + end + "] => " + "[" + newStart + "," + end + "]");
-        start = newStart;
-        found = true;         
-        break; 
-      }  
+  let found = false;
+  for (let j = 0; j < tt.length; j++) {
+    let oo = tt[j];
+    let maxValue = (oo[1] + oo[2]);
+    let maxEnd = (start + end) - 1;
+    if (start >= oo[1] && maxEnd < maxValue) {
+      let offset = (start - oo[1]);
+      let newStart = oo[0] + offset;
+
+      console.log(transformOrder[aTransform] + ": [" + start + "," + end + "] => " + "[" + newStart + "," + end + "]");
+      found = true;
+      TransformInterval(newStart, end, aTransformMap, aTransformOrder, aTransform + 1, aMin);
+      break;
     }
-    if (!found)
-      console.log(transformOrder[i] + ": [" + start + "," + end + "] => " + "[" + start + "," + end + "]");
+    else if (oo[1] >= start && maxValue < maxEnd) {
+      TransformInterval(oo[0], oo[2], aTransformMap, aTransformOrder, aTransform + 1, aMin);
+      found = true;
+    }
+    else if (start >= oo[1] && start <= maxValue && maxEnd >= maxValue) {
+      let offset = (start - oo[1]);
+      let newStart = oo[0] + offset;
+
+      let newEnd = maxValue - start;
+      console.log(transformOrder[aTransform] + ": [" + start + "," + newEnd + "] => " + "[" + newStart + "," + newEnd + "]");
+      found = true;
+      TransformInterval(newStart, newEnd, aTransformMap, aTransformOrder, aTransform + 1, aMin);
+    }
+    else if (maxEnd >= oo[1] && maxEnd < maxValue) {
+      let newStart = oo[0];
+
+      let newEnd = maxEnd - oo[1];
+      end -= newEnd;
+
+      console.log(transformOrder[aTransform] + ": [" + oo[1] + "," + newEnd + "] => " + "[" + newStart + "," + newEnd + "]");
+      found = true;
+      TransformInterval(newStart, newEnd, aTransformMap, aTransformOrder, aTransform + 1, aMin);
+    }
   }
+  if (!found) {
+    console.log(transformOrder[aTransform] + ": [" + start + "," + end + "] => " + "[" + start + "," + end + "]");
+    TransformInterval(aSeedStart, aSeedEnd, aTransformMap, aTransformOrder, aTransform + 1, aMin);
+  }
+}
+
+function TransformSeeds2(aSeeds, aTransformMap, aTransformOrder) {
+  let mm = { min: Number.MAX_SAFE_INTEGER };
+  for (let i = 0; i < aSeeds.length; i += 2)
+    TransformInterval(aSeeds[i], aSeeds[i + 1], aTransformMap, aTransformOrder, 0, mm);
+  return mm.min;
 }
 
 let transformOrder = [];
 
 let seeds = [];
 let transformMap = new Map();
-util.MapInput("./Day5TestInput.txt", (aElem, aIndex) => {
-  if (aIndex == 0)
-  {
-    seeds = aElem.split(" ").map((aElem, aIndex)=>{ 
-      if (aIndex == 0) 
-        return 0; 
-      else 
+util.MapInput("./Day5Input.txt", (aElem, aIndex) => {
+  if (aIndex == 0) {
+    seeds = aElem.split(" ").map((aElem, aIndex) => {
+      if (aIndex == 0)
+        return 0;
+      else
         return parseInt(aElem);
-      });
-    seeds.shift();    
+    });
+    seeds.shift();
   }
-  else
-  {
-    let tt = aElem.split("\r\n").map((aElem, aIndex)=>{
+  else {
+    let tt = aElem.split("\r\n").map((aElem, aIndex) => {
       if (aIndex == 0)
         return aElem.split(" ")[0];
-      else
-      {
-       let pp = aElem.split(" ").map((aElem)=>{
+      else {
+        let pp = aElem.split(" ").map((aElem) => {
           return parseInt(aElem);
         })
-        return pp;      
+        return pp;
       }
     });
-    
+
     transformOrder.push(tt[0]);
     transformMap.set(tt[0], tt.slice(1));
-  }  
+  }
 }, "\r\n\r\n");
 
 console.log(seeds);
@@ -112,4 +146,4 @@ console.log(transformOrder);
 
 console.log(TransformSeeds(seeds, transformMap, transformOrder));
 
-TransformInterval(79, 14, transformMap, transformOrder);
+console.log(TransformSeeds2(seeds, transformMap, transformOrder));
