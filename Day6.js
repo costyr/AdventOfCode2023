4ddbed62b4492489d3d6b9cbae7c2d63722ec312
Costi Rada <costi.rada@caphyon.com>
2023-12-06 08:21:12 +0200

    Day 6 problem solutions

--- Day6.js
+++ Day6.js
@@ -1,5 +1,26 @@
 const util = require('./Util.js');
 
+function ComputeWinFactor(aTime, aDistance) {
+
+  let total = 1;
+  for (let i = 0; i < aTime.length; i++)
+  {
+    let winCount = 0;
+    for (let j = 1; j < aTime[i]; j++)
+    {
+      let dist = (aTime[i] - j) * j;
+      if (dist > aDistance[i])
+        winCount++; 
+    }
+    
+    console.log(winCount);
+    total *= winCount;
+  }
+
+  
+  return total;
+}
+
 let time = [];
 let distance = [];
 util.MapInput("./Day6Input.txt", (aElem, aIndex) => {
@@ -22,3 +43,10 @@ util.MapInput("./Day6Input.txt", (aElem, aIndex) => {
 
 console.log(time);
 console.log(distance);
+
+console.log(ComputeWinFactor(time, distance));
+
+let time2 = [ 40929790];
+let dist2 = [ 215106415051100];
+
+console.log(ComputeWinFactor(time2, dist2));
