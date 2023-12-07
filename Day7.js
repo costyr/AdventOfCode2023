60e1af8231249c25fabdc77a8fe4771645ea84bb
Costi Rada <costi.rada@caphyon.com>
2023-12-07 13:55:21 +0200

    Code refactoring

--- Day7.js
+++ Day7.js
@@ -3,8 +3,8 @@ const util = require('./Util.js');
 const kCardOrder = ['2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A'];
 const kCardOrder2 = ['J', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'Q', 'K', 'A'];
 
-function ComputeType(aCards) {
-  let nn = new Map();
+function MapByFrequency(aCards) {
+  let cardFrequency = new Map();
 
   aCards.split("").reduce((aTypeMap, aElem) => {
     let pp = aTypeMap.get(aElem);
@@ -14,76 +14,98 @@ function ComputeType(aCards) {
       aTypeMap.set(aElem, { count: 1 });
 
     return aTypeMap;
-  }, nn);
+  }, cardFrequency);
 
-  if (nn.size == 1)
+  return cardFrequency;
+}
+
+function ComputeTypeFromFrequency(aCardFrequencyMap) {
+  if (aCardFrequencyMap.size == 1)
     return 7;
-  else if (nn.size == 2) {
-    for (let [key, value] of nn)
+  else if (aCardFrequencyMap.size == 2) {
+    for (let [key, value] of aCardFrequencyMap)
       if (value.count == 4)
         return 6;
     return 5;
   }
-  else if (nn.size == 3) {
-    for (let [key, value] of nn)
+  else if (aCardFrequencyMap.size == 3) {
+    for (let [key, value] of aCardFrequencyMap)
       if (value.count == 3)
         return 4;
     return 3;
   }
-  else if (nn.size == 4)
+  else if (aCardFrequencyMap.size == 4)
     return 2;
-  else if (nn.size == 5)
+  else if (aCardFrequencyMap.size == 5)
     return 1;
 }
 
+function ComputeType(aCards) {
+  return ComputeTypeFromFrequency(MapByFrequency(aCards));
+}
+
 function ComputeType2(aCards) {
-  let nn = new Map();
+  let freqMap = MapByFrequency(aCards);
 
-  aCards.split("").reduce((aTypeMap, aElem) => {
-    let pp = aTypeMap.get(aElem);
-    if (pp != undefined)
-      pp.count++;
-    else
-      aTypeMap.set(aElem, { count: 1 });
-
-    return aTypeMap;
-  }, nn);
-
-  let jokers = nn.get('J');
+  let jokers = freqMap.get('J');
 
   let jokersCount = (jokers != undefined) ? jokers.count : 0;
 
   let max = 0;
   let maxCard;
-  for (let [key, value] of nn)
+  for (let [key, value] of freqMap)
     if (key != 'J' && value.count > max) {
       max = value.count;
       maxCard = key;
     }
 
   if (maxCard != undefined && jokersCount > 0) {
-    nn.set(maxCard, { count: max + jokersCount });
-    nn.delete('J');
+    freqMap.set(maxCard, { count: max + jokersCount });
+    freqMap.delete('J');
   }
 
-  if (nn.size == 1)
-    return 7;
-  else if (nn.size == 2) {
-    for (let [key, value] of nn)
-      if (value.count == 4)
-        return 6;
-    return 5;
-  }
-  else if (nn.size == 3) {
-    for (let [key, value] of nn)
-      if (value.count == 3)
-        return 4;
-    return 3;
-  }
-  else if (nn.size == 4)
-    return 2;
-  else if (nn.size == 5)
-    return 1;
+  return ComputeTypeFromFrequency(freqMap);
+}
+
+function ComputeScore(aAllCards) {
+  let score = aAllCards.reduce((aTotal, aElem, aIndex) => {
+    return aTotal + (aIndex + 1) * aElem.score;
+  }, 0);
+  return score;
+}
+
+function SortAndComputeScore(aAllCards) {
+  aAllCards.sort((a, b) => {
+
+    if (a.type == b.type) {
+      for (let i = 0; i < a.cards.length; i++)
+        if (a.cards[i] != b.cards[i])
+          return kCardOrder.indexOf(a.cards[i]) - kCardOrder.indexOf(b.cards[i]);
+
+      return 0;
+    }
+
+    return a.type - b.type;
+  });
+
+  return ComputeScore(aAllCards);
+}
+
+function SortAndComputeScore2(aAllCards) {
+  aAllCards.sort((a, b) => {
+
+    if (a.type2 == b.type2) {
+      for (let i = 0; i < a.cards.length; i++)
+        if (a.cards[i] != b.cards[i])
+          return kCardOrder2.indexOf(a.cards[i]) - kCardOrder2.indexOf(b.cards[i]);
+
+      return 0;
+    }
+
+    return a.type2 - b.type2;
+  });
+
+  return ComputeScore(aAllCards);
 }
 
 let allCards = util.MapInput("./Day7Input.txt", (aElem) => {
@@ -92,9 +114,9 @@ let allCards = util.MapInput("./Day7Input.txt", (aElem) => {
 
   let cards = cardAndScore[0].split("");
 
-  let type2 = ComputeType(cardAndScore[0]);
+  let type = ComputeType(cardAndScore[0]);
 
-  let type = ComputeType2(cardAndScore[0]);
+  let type2 = ComputeType2(cardAndScore[0]);
 
   let score = parseInt(cardAndScore[1]);
 
@@ -102,23 +124,5 @@ let allCards = util.MapInput("./Day7Input.txt", (aElem) => {
 
 }, "\r\n");
 
-allCards.sort((a, b) => {
-
-  if (a.type == b.type) {
-    for (let i = 0; i < a.cards.length; i++)
-      if (a.cards[i] != b.cards[i])
-        return kCardOrder2.indexOf(a.cards[i]) - kCardOrder2.indexOf(b.cards[i]);
-
-    return 0;
-  }
-
-  return a.type - b.type;
-});
-
-console.log(allCards);
-
-let sum = 0;
-for (let i = 0; i < allCards.length; i++)
-  sum += (i + 1) * allCards[i].score;
-
-console.log(sum);
+console.log(SortAndComputeScore(allCards));
+console.log(SortAndComputeScore2(allCards));
