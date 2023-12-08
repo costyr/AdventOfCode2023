045c936d598061c9f1d6dedcd2299cd481002c33
Costi Rada <costi.rada@caphyon.com>
2023-12-08 11:11:44 +0200

    Day 8 second problem solution

--- Day8.js
+++ Day8.js
@@ -1,30 +1,58 @@
 const util = require('./Util.js');
 
+function FindEndings(aEnd, aDirsMap) {
+  let allStarts = [];
+  for (let [key, value] of aDirsMap)
+  {
+    if (key[key.length - 1] == aEnd)
+      allStarts.push(key);
+  }
 
-function FindZZZ(aInsts, aDirsMap) {
+  return allStarts;
+}
 
-  let cc = "AAA";
+function FindZZZ(aStarts, aEnd2, aInsts, aDirsMap) {
+
+  let cc = aStarts;
   let i = 0;
   let count = 0;
+  let prevCount = 0;
   for(;;)
   {
-    if (cc == "ZZZ")
-      break;
+    let tt = 0;
+    for (let j = 0; j < cc.length; j++) {
+      if ((aEnd2 && cc[j][cc[j].length - 1] =='Z') || 
+          (cc[j] == "ZZZ"))
+        tt++;
+    }
+
+    if (tt == cc.length)
+    {
+      if (prevCount == count)
+        break;
+      else {
+       prevCount = count;
+       count = 0;
+      }
+    }
 
     let ii = aInsts[i];
 
-    let yy = aDirsMap.get(cc);
+    for (let j = 0; j < cc.length; j++) {
+      let yy = aDirsMap.get(cc[j]);
 
-    if (ii == 'L')
-      cc = yy.l;
-    else
-      cc = yy.r;
+      if (ii == 'L')
+        cc[j] = yy.l;
+      else
+        cc[j] = yy.r;
+    }
     
     i++;
     if (i >= aInsts.length)
       i = 0;
 
     count ++;
+    //console.log(cc + " " + tt);
   }
 
   return count;
@@ -33,7 +61,7 @@ function FindZZZ(aInsts, aDirsMap) {
 let insts = "";
 let dirsMap = new Map();
 
-util.MapInput("./Day8Input.txt", (aElem, aIndex) => {
+util.MapInput("./Day8TestInput2.txt", (aElem, aIndex) => {
   if (aIndex == 0)
     insts = aElem;
   else if (aIndex == 1)
@@ -53,4 +81,25 @@ util.MapInput("./Day8Input.txt", (aElem, aIndex) => {
 console.log(insts);
 console.log(dirsMap);
 
-console.log(FindZZZ(insts, dirsMap));
+//console.log(FindZZZ(["AAA"], false, insts, dirsMap));
+
+let allStarts = FindEndings('A', dirsMap);
+let allEnds = FindEndings('Z', dirsMap);
+
+console.log(allStarts);
+console.log(allEnds);
+
+let vv = [];
+for (let i = 0; i < allStarts.length; i++) {
+  let oo = FindZZZ([allStarts[i]], true, insts, dirsMap);
+  console.log(allStarts[i] + " " + oo);
+  vv.push(oo);
+}
+
+const lcm = (...arr) => {
+  const gcd = (x, y) => (!y ? x : gcd(y, x % y));
+  const _lcm = (x, y) => (x * y) / gcd(x, y);
+  return [...arr].reduce((a, b) => _lcm(a, b));
+};
+
+console.log(lcm(vv));
